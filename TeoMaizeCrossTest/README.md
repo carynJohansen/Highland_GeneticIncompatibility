0512d50339faa57ce5b89ac1dd42f216dd0cc3e4
carynJohansen <cjohansen@ucdavis.edu>
2017-09-15 16:22:43 -0700

    sra-dump.sh and README

---
+++ TeoMaizeCrossTest/README.md
@@ -0,0 +1,16 @@
+# Testing the Gene Incompatibility hypothesis/analysis pipeline on Maize:Teosinte data
+
+The purpose of this example project is to use RNA-Seq data from the paper Lemmon et al. 2014.
+
+Paper found here: http://journals.plos.org/plosgenetics/article?id=10.1371/journal.pgen.1004745
+
+Lemmon et al. made crosses between teosinte inbred lines and maize inbred lines.
+
+I'm downloading some of the F1 RNA-Seq data, and working on the analysis pipeline.
+
+Data information
+
+SRA project: https://www.ncbi.nlm.nih.gov/bioproject/PRJNA262181
+
+
+
--- TeoMaizeCrossTest/scripts/sra-dump.sh
+++ TeoMaizeCrossTest/scripts/sra-dump.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
 
-#SBATCH -D /home/caryn89/Projects/Highland_GeneticIncompatibility 
-#SBATCH -o /home/caryn89/Projects/Highland_GeneticIncompatibility/logs/sra-dump-%j.out
-#SBATCH -e /home/caryn89/Projects/Highland_GeneticIncompatibility/logs/sra-dump-%j.out
+#SBATCH -D /home/caryn89/Projects/Highland_GeneticIncompatibility/TeoMaizeCrossTest/ 
+#SBATCH -o /home/caryn89/Projects/Highland_GeneticIncompatibility/TeoMaizeCrossTest/logs/sra-dump-%j.out
+#SBATCH -e /home/caryn89/Projects/Highland_GeneticIncompatibility/TeoMaizeCrossTest/logs/sra-dump-%j.out
 #SBATCH -J sra-dump
-#SBATCH --time=10:00:00
+#SBATCH --time=5:00:00
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=cjohansen@ucdavis.edu
 #SBATCH --mem=5000
@@ -24,14 +24,14 @@ srrFile="srr_numbers.txt"
 srr=$(awk -v var="$SLURM_ARRAY_TASK_ID" 'FNR == var {print}' $srrFile)
 echo $SLURM_ARRAY_TASK_ID $srr >> sra_info.txt
 
-fastq-dump --outdir data/raw --gzip --skip-technical --readids --read-filter pass --dumpbase --split-files --clip $srr
+outdir="/home/caryn89/Projects/Highland_GeneticIncompatibility/TeoMaizeCrossTest/data/raw"
 
-srr="SRR2541179"
+prefetch $srr && fastq-dump --outdir data/raw --gzip --skip-technical --readids --read-filter pass --dumpbase --split-files --clip $srr
 
-fastq-dump --outdir data/seq --gzip --skip-technical --readids --read-filter pass --dumpbase --split-files --clip $srr
 exitError=$?
-echo exit error $x
+echo exit error $exitError
 
 vbd-validate $srr
 exitError=$?
-echo exit error: $x
+echo exit error: $exitError
+
