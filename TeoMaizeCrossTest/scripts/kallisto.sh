4000aebb475517d3225d10dae495e0a49d4930c7
carynJohansen <cjohansen@ucdavis.edu>
2017-09-17 15:52:07 -0700

    added run time calculations for kalliso index and quant

--- TeoMaizeCrossTest/scripts/kallisto.sh
+++ TeoMaizeCrossTest/scripts/kallisto.sh
@@ -4,7 +4,7 @@
 #SBATCH -J kallisto
 #SBATCH -o /home/caryn89/Projects/Highland_GeneticIncompatibility/TeoMaizeCrossTest/logs/kallisto_%j.out
 #SBATCH -e /home/caryn89/Projects/Highland_GeneticIncompatibility/TeoMaizeCrossTest/logs/kallisto_%j.out
-#SBATCH --time=4:00:00
+#SBATCH --time=9:00:00
 #SBATCH --mem=50000
 #SBATCH --array=1-13
 
@@ -36,15 +36,28 @@ echo $SLURM_ARRAY_TASK_ID $srr $fasta $fastq $idx >> kallisto_info.txt
 
 #Build index
 
+idx_start=`date +%s`
+
 kallisto index -i data/raw/$idx data/raw/$fasta
 err=$?
 echo index error: $err
 
+idx_end=`date +%s`
+((idx_time=$end-$start))
+
+echo kallisto index runtime was $idx_time s
+
 # Quantify
+
+quant_start=`date +%s`
 
 mkdir data/processed/$srr
 
 kallisto quant -i data/raw/$idx -o data/processed/$srr -b 100 --single -l 101 -s 20 data/raw/$fastq
 err=$?
 echo kallisto error: $err
+
+quant_end=`date +%s`
+((quant_time=$quant_end - $quant_start))
+echo kallisto quantification run time was $quant_time s
 
