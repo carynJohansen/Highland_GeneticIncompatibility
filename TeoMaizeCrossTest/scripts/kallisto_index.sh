14589f9b6a2dd311795654c6c50467015ff83a63
carynJohansen <cjohansen@ucdavis.edu>
2017-09-18 11:08:51 -0700

    make index for gene model

---
+++ TeoMaizeCrossTest/scripts/kallisto_index.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -l
+
+#SBATCH -D /home/caryn89/Projects/Highland_GeneticIncompatibility/TeoMaizeCrossTest
+#SBATCH -J index
+#SBATCH -o /home/caryn89/Projects/Highland_GeneticIncompatibility/TeoMaizeCrossTest/logs/index_%j.out
+#SBATCH -e /home/caryn89/Projects/Highland_GeneticIncompatibility/TeoMaizeCrossTest/logs/index_%j.out
+#SBATCH --time=9:00:00
+#SBATCH --mem=50000
+
+set -u
+set -e
+
+## MODULES
+
+module load kallisto
+
+#print version
+kallisto version
+
+## DATA
+
+cdna="/home/caryn89/genomes/maize_cdna_v4/Zea_mays.AGPv4.cdna.all.fa.gz"
+
+idx="/home/caryn89/genomes/maize_cdna_v4/Zea_mays.AGPv4.cdna.all.idx"
+
+
+## MAIN
+
+#Build index
+
+idx_start=`date +%s`
+
+kallisto index -i $idx $cdna
+err=$?
+echo index error: $err
+
+idx_end=`date +%s`
+((idx_time=$end-$start))
