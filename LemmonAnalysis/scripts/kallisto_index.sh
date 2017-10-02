adda85f770512aa1d5c0bca947e5089e26ca2cc6
carynJohansen <cjohansen@ucdavis.edu>
2017-10-02 09:31:33 -0700

    change dir name to Lemmon

---
+++ LemmonAnalysis/scripts/kallisto_index.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -l
+
+#SBATCH -D /home/caryn89/Projects/Highland_GeneticIncompatibility/TeoMaizeCrossTest
+#SBATCH -J index
+#SBATCH -o /home/caryn89/Projects/Highland_GeneticIncompatibility/TeoMaizeCrossTest/logs/index_%j.out
+#SBATCH -e /home/caryn89/Projects/Highland_GeneticIncompatibility/TeoMaizeCrossTest/logs/index_%j.out
+#SBATCH --time=5:00:00
+#SBATCH --mem=50000
+
+set -u
+set -e
+
+## MODULES
+
+module load kallisto
+
+#print version
+kallisto version
+
+## DATA
+
+cdna="/home/caryn89/genomes/maize_cdna_v4/Zea_mays.AGPv4.cdna.T01filtered.fa"
+
+idx="/home/caryn89/genomes/maize_cdna_v4/Zea_mays.AGPv4.cdna.T01filtered.idx"
+
+
+## MAIN
+
+#Build index
+
+idx_start=`date +%s`
+
+kallisto index -i $idx $cdna
+err=$?
+echo index error: $err
+
+idx_end=`date +%s`
+((idx_time=$idx_end-$idx_start))
+echo $idx_time
+
