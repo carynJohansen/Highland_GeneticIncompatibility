e8cdee2256c29189f4a9cadb03a4f403f800d0ab
carynJohansen <cjohansen@ucdavis.edu>
2017-10-04 13:18:16 -0700

    kallisto for v3

--- LemmonAnalysis/scripts/kallisto.sh
+++ LemmonAnalysis/scripts/kallisto.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 
 #SBATCH -D /home/caryn89/Projects/Highland_GeneticIncompatibility/LemmonAnalysis
-#SBATCH -J kallisto
-#SBATCH -o /home/caryn89/Projects/Highland_GeneticIncompatibility/LemmonAnalysis/logs/T01_kallisto_%j.out
-#SBATCH -e /home/caryn89/Projects/Highland_GeneticIncompatibility/LemmonAnalysis/logs/T01_kallisto_%j.out
+#SBATCH -J kallisto_v3
+#SBATCH -o /home/caryn89/Projects/Highland_GeneticIncompatibility/LemmonAnalysis/logs/v3_kallisto_%j.out
+#SBATCH -e /home/caryn89/Projects/Highland_GeneticIncompatibility/LemmonAnalysis/logs/v3_kallisto_%j.out
 #SBATCH --time=9:00:00
 #SBATCH --mem=50000
 #SBATCH --array=1-13
@@ -25,7 +25,7 @@ srrFile="srr_numbers.txt"
 srr=$(awk -v var="$SLURM_ARRAY_TASK_ID" 'FNR == var {print}' $srrFile)
 
 
-idx="/home/caryn89/genomes/maize_cdna_v4/Zea_mays.AGPv4.cdna.idx"
+idx="/home/caryn89/genomes/maize_cdna_v3/Zea_mays.AGPv3.cdna.all.fa.idx"
 
 fastq=$srr\_pass\_1.fastq.gz
 
@@ -36,9 +36,9 @@ fastq=$srr\_pass\_1.fastq.gz
 quant_start=`date +%s`
 
 
-mkdir data/processed/v4/$srr
+mkdir data/processed/v3/bootstrap100/$srr
 
-kallisto quant -i $idx -o data/processed/v4/$srr -b 50 --single -l 101 -s 20 data/raw/$fastq
+kallisto quant -i $idx -o data/processed/v3/bootstrap100/$srr -b 100 --single -l 101 -s 20 data/raw/$fastq
 
 err=$?
 echo kallisto error: $err
@@ -47,5 +47,5 @@ quant_end=`date +%s`
 ((quant_time=$quant_end - $quant_start))
 echo kallisto quantification run time was $quant_time s
 
-echo $SLURM_JOB_ID $SLURM_ARRAY_TASK_ID $quant_time $srr $fastq $idx >> b100_kallisto_info.txt
+echo $SLURM_JOB_ID $SLURM_ARRAY_TASK_ID $quant_time $srr $fastq $idx >> v3_b100_kallisto_info.txt
 
