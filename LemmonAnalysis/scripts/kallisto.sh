72bcafe5f3f514d7f7f5c14176aa392bb2695f14
carynJohansen <cjohansen@ucdavis.edu>
2017-10-04 13:13:49 -0700

    fixing other merge conflicts

--- LemmonAnalysis/scripts/kallisto.sh
+++ LemmonAnalysis/scripts/kallisto.sh
@@ -24,11 +24,9 @@ srrFile="srr_numbers.txt"
 
 srr=$(awk -v var="$SLURM_ARRAY_TASK_ID" 'FNR == var {print}' $srrFile)
 
-<<<<<<< HEAD
+
 idx="/home/caryn89/genomes/maize_cdna_v4/Zea_mays.AGPv4.cdna.idx"
-=======
-idx="/home/caryn89/genomes/maize_cdna_v4/Zea_mays.AGPv4.cdna.all.idx"
->>>>>>> d8c150c7bc1084464ec375f80503cfe2d93fb730
+
 fastq=$srr\_pass\_1.fastq.gz
 
 ## MAIN
@@ -37,15 +35,11 @@ fastq=$srr\_pass\_1.fastq.gz
 
 quant_start=`date +%s`
 
-<<<<<<< HEAD
+
 mkdir data/processed/v4/$srr
 
 kallisto quant -i $idx -o data/processed/v4/$srr -b 50 --single -l 101 -s 20 data/raw/$fastq
-=======
-mkdir data/bootstrap100/$srr
 
-kallisto quant -i $idx -o data/bootstrap100/$srr -b 100 --single -l 101 -s 20 data/raw/$fastq
->>>>>>> d8c150c7bc1084464ec375f80503cfe2d93fb730
 err=$?
 echo kallisto error: $err
 
