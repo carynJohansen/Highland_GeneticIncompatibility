f0a39ad55a71e72e268df3776076105ac6f5a776
carynJohansen <cjohansen@ucdavis.edu>
2017-10-09 15:03:05 -0700

    editing sra dump for all leaf samples

--- LemmonAnalysis/scripts/sra-dump.sh
+++ LemmonAnalysis/scripts/sra-dump.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -l
 
 #SBATCH -D /home/caryn89/Projects/Highland_GeneticIncompatibility/LemmonAnalysis/ 
-#SBATCH -o /home/caryn89/Projects/Highland_GeneticIncompatibility/LemmonAnalysis/logs/sra-dump-%j.out
-#SBATCH -e /home/caryn89/Projects/Highland_GeneticIncompatibility/LemmonAnalysis/logs/sra-dump-%j.out
+#SBATCH -o /home/caryn89/Projects/Highland_GeneticIncompatibility/LemmonAnalysis/logs/sra-leaves-dump-%j.out
+#SBATCH -e /home/caryn89/Projects/Highland_GeneticIncompatibility/LemmonAnalysis/logs/sra-leaves-dump-%j.out
 #SBATCH -J fasta
 #SBATCH --time=5:00:00
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=cjohansen@ucdavis.edu
 #SBATCH --mem=4000
-#SBATCH --array=1-13
+#SBATCH --array=1-151
 
 set -u
 set -e
@@ -19,12 +19,12 @@ module load sratoolkit/2.8.2
 
 ## DATA
 
-srrFile="srr_numbers.txt"
+srrFile="leaf_srr_numbers.txt"
 
 srr=$(awk -v var="$SLURM_ARRAY_TASK_ID" 'FNR == var {print}' $srrFile)
-echo $SLURM_ARRAY_TASK_ID $srr >> sra_info.txt
+echo $SLURM_JOB_ID $SLURM_ARRAY_TASK_ID $srr >> leaves_sra_info.txt
 
-outdir="/home/caryn89/Projects/Highland_GeneticIncompatibility/TeoMaizeCrossTest/data/raw"
+outdir="/home/caryn89/Projects/Highland_GeneticIncompatibility/LemmonAnalysis/data/raw"
 
 #fastq-dump --outdir data/raw --gzip --skip-technical --readids --read-filter pass --dumpbase --split-files --clip $srr
 
