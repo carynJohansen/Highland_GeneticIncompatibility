f6fd99c44aa399ff788ce3bc1fdae46efd6d989e
carynJohansen <cjohansen@ucdavis.edu>
2017-10-09 12:21:36 -0700

    kallisto script edits to analyze all leaf RNA seq data

--- LemmonAnalysis/scripts/kallisto.sh
+++ LemmonAnalysis/scripts/kallisto.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
 
 #SBATCH -D /home/caryn89/Projects/Highland_GeneticIncompatibility/LemmonAnalysis
-#SBATCH -J full_project
-#SBATCH -o /home/caryn89/Projects/Highland_GeneticIncompatibility/LemmonAnalysis/logs/proj_kallisto_%j.out
-#SBATCH -e /home/caryn89/Projects/Highland_GeneticIncompatibility/LemmonAnalysis/logs/proj_kallisto_%j.out
+#SBATCH -J leaves
+#SBATCH -o /home/caryn89/Projects/Highland_GeneticIncompatibility/LemmonAnalysis/logs/leaf_kallisto_%j.out
+#SBATCH -e /home/caryn89/Projects/Highland_GeneticIncompatibility/LemmonAnalysis/logs/leaf_kallisto_%j.out
 #SBATCH --time=9:00:00
 #SBATCH --mem=50000
-#SBATCH --array=1-448
+#SBATCH --array=1-151
 
 set -u
 set -e
@@ -20,7 +20,7 @@ kallisto version
 
 ## DATA
 
-srrFile="all_srr_numbers.txt"
+srrFile="leaf_srr_numbers.txt"
 
 srr=$(awk -v var="$SLURM_ARRAY_TASK_ID" 'FNR == var {print}' $srrFile)
 
@@ -40,9 +40,9 @@ echo $idx
 quant_start=`date +%s`
 
 
-mkdir data/processed/v4/PRJNA262181
+mkdir data/processed/v4/PRJNA262181_leaf/$srr
 
-kallisto quant -i $idx -o data/processed/v4/PRJNA262181/$srr -b 100 --single -l 101 -s 20 data/raw/$fastq
+kallisto quant -i $idx -o data/processed/v4/PRJNA262181_leaf/$srr -b 100 --single -l 101 -s 20 data/raw/$fastq
 
 err=$?
 echo kallisto error: $err
@@ -51,5 +51,5 @@ quant_end=`date +%s`
 ((quant_time=$quant_end - $quant_start))
 echo kallisto quantification run time was $quant_time s
 
-echo $SLURM_JOB_ID $SLURM_ARRAY_TASK_ID $quant_time $srr >> PRJNA262181_kallisto_info.txt
+echo $SLURM_JOB_ID $SLURM_ARRAY_TASK_ID $quant_time $srr >> PRJNA262181_leaf_kallisto_info.txt
 
