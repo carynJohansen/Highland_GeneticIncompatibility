b3106c12e7b1f7cdeb4936413fd9128e5b9aef3c
carynJohansen <cjohansen@ucdavis.edu>
2017-10-12 15:22:23 -0700

    adding script to analyze using v3 specifically

---
+++ LemmonAnalysis/scripts/kallisto_v3.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -l
+
+#SBATCH -D /home/caryn89/Projects/Highland_GeneticIncompatibility/LemmonAnalysis
+#SBATCH -J leaves
+#SBATCH -o /home/caryn89/Projects/Highland_GeneticIncompatibility/LemmonAnalysis/logs/leaf_kallisto_%j.out
+#SBATCH -e /home/caryn89/Projects/Highland_GeneticIncompatibility/LemmonAnalysis/logs/leaf_kallisto_%j.out
+#SBATCH --time=9:00:00
+#SBATCH --mem=50000
+#SBATCH --array=1-151
+
+set -u
+set -e
+
+## MODULES
+
+module load kallisto
+
+#print version
+kallisto version
+
+## DATA
+
+srrFile="leaf_srr_numbers.txt"
+
+srr=$(awk -v var="$SLURM_ARRAY_TASK_ID" 'FNR == var {print}' $srrFile)
+
+echo $srr
+
+idx="/home/caryn89/genomes/maize_cdna_v4/Zea_mays.AGPv4.cdna.all.idx"
+
+fastq=$srr\_pass\_1.fastq.gz
+
+echo $fastq
+echo $idx
+
+## MAIN
+
+# Quantify
+
+quant_start=`date +%s`
+
+
+mkdir data/processed/v4/PRJNA262181_leaf/$srr
+
+kallisto quant -i $idx -o data/processed/v4/PRJNA262181_leaf/$srr -b 100 --single -l 101 -s 20 data/raw/$fastq
+
+err=$?
+echo kallisto error: $err
+
+quant_end=`date +%s`
+((quant_time=$quant_end - $quant_start))
+echo kallisto quantification run time was $quant_time s
+
+echo $SLURM_JOB_ID $SLURM_ARRAY_TASK_ID $quant_time $srr >> PRJNA262181_leaf_kallisto_info.txt
+
